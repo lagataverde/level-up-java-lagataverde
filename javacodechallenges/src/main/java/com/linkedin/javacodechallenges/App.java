a9ec19206a57af6fbb2544c5cd15bfff666f4cda
lagataverde <catalinalvd@gmail.com>
2024-05-20 19:59:51 +0000

    Implement calculateWaterBill challenge

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,9 +4,21 @@ import java.util.Scanner;
 
 public class App {
 
+    public static final double CCF = 748;
+    public static final double MINIMUM_CHARGE = 18.84;
+    public static final double MINIMUM_GALLONS = 1496;
+    public static final double CCF_COST = 3.90;
+
     public static double calculateWaterBill(double gallonsUsage) {
-        // TODO: Implement method
-        return 0;
+        
+        if (gallonsUsage <= MINIMUM_GALLONS) {
+            return MINIMUM_CHARGE;
+        }
+
+        double additionalCffsUsed = Math.ceil((gallonsUsage - MINIMUM_GALLONS)/CCF); 
+        double additionalCost = additionalCffsUsed * CCF_COST;
+
+        return MINIMUM_CHARGE + additionalCost;
     }
 
     public static void main(String[] args) {
