c47a848647e2bb29428546906cd034a9af9c77fe
lagataverde <catalinalvd@gmail.com>
2024-05-22 20:48:49 +0000

    Implement redacted file challenge

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,12 +1,56 @@
 package com.linkedin.javacodechallenges;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
 
-public class App 
-{
+public class App {
+    public static final String REDACTED_FILE = "redactedFile.txt";
+    public static final String REDACTED = "REDACTED";
+
     public static void redactTextFile(String fileName,
-                                      String[] redactedWordsArray) {
+            String[] wordsToredactArray) {
+        try {
+            File file = new File(fileName);
 
+            if (file.canRead()) {
+                FileReader fileReader = new FileReader(file);
+                FileWriter fileWriter = new FileWriter(REDACTED_FILE);
+
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+                List<String> wordsToRedactList = Arrays.asList(wordsToredactArray);
+                String line;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    Iterator<String> iterator = wordsToRedactList.iterator();
+                    while (iterator.hasNext()) {
+                        line = StringUtils.replaceIgnoreCase(line, iterator.next(), REDACTED);
+                    }
+
+                    try {
+                        bufferedWriter.write(line + "\n");
+                    } catch (IOException e) {
+                        System.out.println("Unable to write word to file.");
+                        e.printStackTrace();
+                    }
+                }
+                bufferedReader.close();
+                bufferedWriter.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Redacted file creation has failed.");
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
